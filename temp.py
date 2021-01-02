bbefdd9deffc0f94e10b374ca531a47bd96ed5c7
amitjpatil23 <amitjpatil23@gmail.com>
2021-01-03 00:37:25 +0530

    my first commit

---
+++ temp.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print('hello world')
+a=5
+b=7
+c=a+b
+print(c)
+a=76
+b=77
+c=a-b
+print(c)
+
+
+name="amit patil"
+company="ipm"
+print("my name is",name+" and i work at",company)
